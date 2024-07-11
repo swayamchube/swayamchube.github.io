e90e6fa55cc235472b438150088da34146445386
Swayam Chube <chubeswayam1701@gmail.com>
2024-07-11 22:58:58 +0530

    been a while

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MarkdownComponent } from 'ngx-markdown/src';
 
 @Component({
   selector: 'app-home',
---
+++ src/assets/main.md
@@ -0,0 +1,2 @@
+# Galois Theory
+Let $K/k$ be a Galois extension with galois group $Gal(K/k)$.
